be6b814a109cc7cc46b69195920b6c573ac465fa
Siddhartha Banerjee <sidban@uwalumni.com>
2020-03-01 13:30:59 -0800

    Mandelbrot image
    Image of Mandelbrot set is added and coloring options are also added.

--- mathlib/fractals.py
+++ mathlib/fractals.py
@@ -6,8 +6,9 @@ Classes for fractals
 """
 
 import numpy as np
-from math import log, log2
+from math import log, log2, floor, ceil
 from PIL import Image, ImageDraw
+from collections import defaultdict
 
 # GLOBAL CONSTANTS
 _MAX_ITER = 80  # Potential function cutoff
@@ -24,16 +25,16 @@ _IM_END = 1
 class Multibrot:
     """Multibrot fractals."""
 
-
     def __init__(
-        self,
-        n: int = 2,
-        max_iter: int = _MAX_ITER,
+            self,
+            n: int = 2,
+            alpha: float = 1.0,
+            max_iter: int = _MAX_ITER,
     ) -> None:
         """Constructor of the class."""
         assert (n >= 2), 'Value of n has to be greater or equal 2'
         self.__n = n
-        self.func = lambda z, c: (z ** self.n + c)
+        self.func = lambda z, c: (z ** self.n + alpha * c)
         self._Re_window = [_RE_START, _RE_END]
         self._Im_window = [_IM_START, _IM_END]
         self._max_iter = max_iter
@@ -55,9 +56,9 @@ class Multibrot:
             raise Exception('Give integer value greater than 1')
 
     def potential(
-        self,
-        c: complex,
-        z0: complex = (0 + 0j),
+            self,
+            c: complex,
+            z0: complex = (0 + 0j),
     ) -> tuple:
         """Gives potential function"""
         z = z0
@@ -75,21 +76,29 @@ class Mandelbrot(Multibrot):
     """Mandelbrot fractal class."""
 
     def __init__(
-        self,
-        max_iter: int = _MAX_ITER,
+            self,
+            n: int = 2,
+            alpha: float = 1.0,
+            max_iter: int = _MAX_ITER,
     ) -> None:
         """Mandelbrot subclass."""
         super().__init__(
+            n=n,
+            alpha=alpha,
             max_iter=max_iter,
         )
 
     def image(
-        self,
-        width: int = _WIDTH,
-        height: int = _HEIGHT,
+            self,
+            width: int = _WIDTH,
+            height: int = _HEIGHT,
+            color: bool = False,
     ) -> Image:
         """Draw fractal image."""
-        im = Image.new('RGB', (width, height), (0, 0, 0))
+        if color:
+            im = Image.new('HSV', (width, height), (0, 0, 0))
+        elif not color:
+            im = Image.new('RGB', (width, height), (0, 0, 0))
         draw = ImageDraw.Draw(im)
 
         for x in range(0, width):
@@ -104,9 +113,21 @@ class Mandelbrot(Multibrot):
                 # Compute the number of iterations
                 m, _ = self.potential(c=c)
                 # The color depends on the number of iterations
-                color = 255 - int(m * 255 / self._max_iter)
-                # Plot the point
-                draw.point([x, y], (color, color, color))
+                # And plot the point
+                if color:
+                    hue = int(255 * m / self._max_iter)
+                    saturation = 255
+                    value = 255 if m < self._max_iter else 0
+                    draw.point([x, y], (hue, saturation, value))
+                elif not color:
+                    value = 255 - int(m * 255 / self._max_iter)
+                    draw.point([x, y], (value, value, value))
 
+        if color:
+            im = im.convert('RGB')
         return im
 
+
+def linear_interpolation(value1, value2, t):
+    """Linear interpolation."""
+    return value1 * (1 - t) + value2 * t
